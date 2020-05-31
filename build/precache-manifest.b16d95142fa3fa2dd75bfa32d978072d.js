5ea568207bbf68f0ed9327316fde31b82a2fcb3f
Sofia Franek <sofiafranek@icloud.com>
2020-05-31 15:52:24 +0100

    Testing deployment in netlify

---
+++ build/precache-manifest.b16d95142fa3fa2dd75bfa32d978072d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7c11cdbe665c28ae69f3c6f4368b60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c1d87fe782ffb10f7c7",
+    "url": "/static/css/main.e8e2f1be.chunk.css"
+  },
+  {
+    "revision": "51edf408ed46cfc78e42",
+    "url": "/static/js/2.c06eada5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c06eada5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1d87fe782ffb10f7c7",
+    "url": "/static/js/main.ab47451f.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  },
+  {
+    "revision": "1af86f05f46db70e0dc24e719472409a",
+    "url": "/static/media/Gordita Regular.1af86f05.woff"
+  },
+  {
+    "revision": "42f999413677f4d23d05d4a84fd5efeb",
+    "url": "/static/media/browse-header.42f99941.jpg"
+  },
+  {
+    "revision": "c0166256039cb58818ba2048e264686c",
+    "url": "/static/media/homepage-header.c0166256.jpg"
+  }
+]);
